b744b15ea2faa1292abb2165c3784b9467050888
SHPI <lh@shpi.de>
2023-12-16 04:20:22 +0000

    added flashing routine for initial setup

--- loading.sh
+++ loading.sh
@@ -1,5 +1,57 @@
 #!/bin/sh -e
 
+# Define ATmega device ID
+ATMEGA_ID="03eb:2ff4"
+ATMEGA_FOUND=0
+MODEL=""
+
+# Read the model from /boot/config.txt
+while read -r line; do
+    case "$line" in
+        model=*)
+            MODEL=${line#model=}
+            ;;
+    esac
+done < "/boot/config.txt"
+
+
+# Proceed only if a model is found
+if [ -n "$MODEL" ]; then
+    # Check if ATMEGA is connected
+    for device in /sys/bus/usb/devices/*; do
+        if [ -f "${device}/idVendor" ] && [ -f "${device}/idProduct" ]; then
+            idVendor=$(cat "${device}/idVendor")
+            idProduct=$(cat "${device}/idProduct")
+            if [ "${idVendor}:${idProduct}" = "$ATMEGA_ID" ]; then
+                ATMEGA_FOUND=1
+                break
+            fi
+        fi
+    done
+
+    # Proceed with flashing only if ATMEGA is found
+    if [ $ATMEGA_FOUND -eq 1 ]; then
+        # Determine which .hex file to flash based on the model
+        case "$MODEL" in
+            *one*)
+                HEX_FILE="one.hex"
+                ;;
+            *zero*)
+                HEX_FILE="zero.hex"
+                ;;
+        esac
+
+        if [ -n "$HEX_FILE" ]; then
+            echo "Flashing $HEX_FILE to ATMEGA device..."
+            dfu-programmer atmega32u4 erase
+            dfu-programmer atmega32u4 flash "/home/pi/shpi_setup/$HEX_FILE"
+            dfu-programmer atmega32u4 start
+        fi
+    fi
+fi
+
+sleep 5
+
 # Calculate the starting column to center the dots (assuming 100 characters per line)
 num_dots=60
 dots_per_row=30
